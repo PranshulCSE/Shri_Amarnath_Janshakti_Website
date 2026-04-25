78a74f04ca3ee1a3acaa575db1848303b712deeb
Pranshul <threjapranshul@gmail.com>
2026-04-25 21:52:38 +0530

    Adding .app in CORS

--- backend/server/server.js
+++ backend/server/server.js
@@ -50,7 +50,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5001',
-  /\.netlify\.app$/,
+  /\.app$/,
   /\.vercel\.app$/,
   /\.onrender\.com$/,
 ];
